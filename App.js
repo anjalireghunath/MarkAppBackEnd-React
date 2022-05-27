7ba20006e7715f5b407870490b6b4bd24d3dc3b5
anjalireghunath <anjalireghu98@gmail.com>
2022-05-27 06:46:35 +0530

    api

---
+++ App.js
@@ -0,0 +1,55 @@
+const Express=require("express")
+const Mongoose=require("mongoose")
+const Bodyparser=require("body-parser")
+
+var app=Express()
+app.use(Bodyparser.urlencoded({extended:true}))
+app.use(Bodyparser.json())
+app.use((req, res, next) => { 
+    res.setHeader("Access-Control-Allow-Origin", "*");  
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"   ); 
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS"   ); 
+    next(); });
+
+var markModel=Mongoose.model("marks",
+new Mongoose.Schema(
+    {
+        name:String,
+        admno:String,
+        cgpa:String
+    })
+)
+Mongoose.connect("mongodb+srv://anjalireghunath:9846434831@cluster0.ursz9.mongodb.net/marksDB")
+
+app.get("/api/viewall",(req,res)=>{
+    markModel.find(
+        (error,data)=>{
+            if(error){
+                 res.send({"status":"error"})
+            }
+            else{
+                res.send(data)
+            }
+        }
+    )
+})
+
+app.post("/api/addmark",(req,res)=>{
+    var data=req.body
+    let ob=new markModel(data)
+    ob.save((error,data)=>{
+        if(error){
+            res.send({"status":"error"})
+        }
+        else{
+            res.send({"status":"success","data":data})
+        }
+    })
+    
+})
+
+
+
+app.listen(4001,()=>{
+    console.log("server running")
+})
